67ee65c5d3d36304590df147937614ecde8112c3
melnikovknst <145424645+melnikovknst@users.noreply.github.com>
2023-10-08 20:05:30 +0700

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
 
+
+int main(void) {
+    char str[80];
+    fgets(str, sizeof(str), stdin);
+    int len = (int)strlen(str);
+    
+    for (int i = 0; i < len - 1; i++) {
+        int count = 1;
+        for (int k = i + 1; k < len; k++) {
+            if (str[i] == str[k])
+                count++;
+        }
+        
+        if (count == 3){
+            printf("%c\n", str[i]);
+            return 0;
+        }
+    }
+    
+    printf("Net takogo simvola\n");
+    return 0;
+}
